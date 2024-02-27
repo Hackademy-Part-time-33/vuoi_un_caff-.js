33cb15d96b7945adeb33f8b3d54a8c3f2aa0c857
Elia Armenti <armentielia@gmail.com>
2024-02-27 18:14:26 +0100

    lavoro svolto

---
+++ newfile2.js
@@ -0,0 +1,85 @@
+// es 1
+/*
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let somma = num1 + num2 + num3 + num4 + num5;
+
+console.log(somma);
+
+let media = somma / 5;
+
+console.log(media);
+
+alert(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`); 
+
+// es 2
+
+let nascita = 1996;
+let corrente = 2024;
+let eta = corrente - nascita;
+let rimanenti = 100 - eta;
+
+console.log(rimanenti);
+
+console.log(corrente - nascita);
+
+alert(`Hai ${corrente - nascita} anni e ti mancano ${rimanenti} anni per compierne 100`)
+
+// es 3
+
+alert('scegli un numero');
+let n1 = parseInt(prompt());
+alert('scegli un numero');
+let n2 = parseInt(prompt());
+
+
+let addizione = n1 + n2;
+let sottrazione = n1 - n2;
+let moltiplicazione = n1 * n2;
+let divisione = n1 / n2;
+let potenza = n1 ** n2;
+
+alert(`ecco il tuo risultato st***o la somma è ${addizione} la sottrazione ${sottrazione} il prodotto è ${moltiplicazione} il risultato è ${divisione} il risultato è ${potenza}`);
+alert*/
+
+// es 4
+
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  console.log(array_1);
+
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+
+  console.log(array_2);
+
+  console.log(array_1[0][0]);
+  console.log(array_1[1][0]);
+  console.log(array_1[0][1]);
+  console.log(array_2[4][0][0]);
+  console.log(array_1[0][0]);
+  console.log(array_1[1][0]);
+  console.log(array_1[0][1]);
+  console.log(array_1[1][1]);
+  console.log(array_1[0][0]);
+  console.log(array_1[1][0]);
+  console.log(array_1[0][1]);
+  console.log(array_1[2][0]);
+  console.log(array_1[2][1])
+  console.log(array_2[3][0]);
+  console.log(array_2[4][1]);
+  console.log(array_1[0][2]);
+
